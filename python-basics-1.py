06ebd9c705d80799a2e1e4c73b1ac8c1906df188
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-12 19:54:40 +0200

    laern python basics

---
+++ python-basics-1.py
@@ -0,0 +1,98 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 5
+2x = 5
+SyntaxError: invalid decimal literal
+x
+5
+x2 = 10
+x2
+10
+x_y = 10
+x_y
+10
+X = 15
+X
+15
+if return yield assert
+SyntaxError: invalid syntax
+#
+type (x)
+<class 'int'>
+f = 'welcome'
+type (f)
+<class 'str'>
+
+h = 5.0
+type (h)
+<class 'float'>
+g = [1,2,3,true]
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    g = [1,2,3,true]
+NameError: name 'true' is not defined. Did you mean: 'True'?
+type (g)
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    type (g)
+NameError: name 'g' is not defined
+g = [1,2,3,4,5,True]
+type (g)
+<class 'list'>
+t = (1,2,3,True)
+type (t)
+<class 'tuple'>
+p = ('welcome')
+type (p)
+<class 'str'>
+<class 'str'>
+SyntaxError: invalid syntax
+j = ('welcome',)
+type (j)
+SyntaxError: multiple statements found while compiling a single statement
+
+j = ('welcome',)
+type (j)
+<class 'tuple'>
+
+
+x = 10
+>>> x == 10
+True
+>>> x == 11
+False
+>>> x !=10
+False
+>>> False
+False
+>>> 
+>>> # if not then it is True
+>>> 
+>>> x = 11
+>>> x = x+1
+>>> x
+12
+>>> x+ = 1
+SyntaxError: invalid syntax
+>>> x += 1
+>>> x
+13
+>>> x *= 3
+>>> x
+39
+>>> x /= 4
+>>> x
+9.75
+>>> # end arithmatic operator
+>>> 
+============ RESTART: C:/Coding/Coding lernen/Python/pyton-basics.py ===========
+x>500
+>>> 
+============ RESTART: C:/Coding/Coding lernen/Python/pyton-basics.py ===========
+-------
+>>> 
+============ RESTART: C:/Coding/Coding lernen/Python/pyton-basics.py ===========
+x> 300
+>>> 
+============ RESTART: C:/Coding/Coding lernen/Python/pyton-basics.py ===========
+x> 300
---
+++ pyton-basics.py
@@ -0,0 +1,18 @@
+x = 400
+
+if x > 500 :
+    print('x>500')
+elif x > 400 :
+    print ('x> 400')
+
+elif x > 300 :
+    print ('x> 300')
+
+elif x > 200 :
+    print ('x> 200')
+
+
+
+
+else:
+    print('x')
