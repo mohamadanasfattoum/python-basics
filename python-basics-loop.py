66c266426c4d3c92dc404ea09399d526f5083bf0
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-17 17:33:44 +0200

    python-basics-loop-break-continue

--- python-basics-loop.py
+++ python-basics-loop.py
@@ -1,19 +1,48 @@
 #while loop
 ...
+#loop with listis
 
+l = [[1,2,3],[4,5,6],[7,8,9]]
+
+
+for x in l:
+    for y in x:
+        print(y)
+print('---------------')
+...
+# while vs for
+
+a = 0
+b = 0
+while a < 4 :
+    a =a+1
+    b =b+1
+    while b < 4 :
+        b = b+1
+        print(a,b)
+print('---------------')        
+...
+# break
 x = 0
 while x < 10 :
+    if x == 5 :
+        break
     print(x)
     x +=1
-    
+print('---------------')
 ...
 
-x = 1
-while x <= 5:
-    y = 1
-    while y <=10 :
-        print(f'{x}X{y} = {x*y}')
-        y +=1
-
-    print('---------------')
-    x += 1
+for x in range(10):
+    if x == 5 :
+        break
+    print(x)
+        
+print('---------------')        
+...
+for x in range(10):
+    if x == 5 :
+        continue
+    print(x)
+        
+print('---------------')        
+...
