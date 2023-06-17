d16914ed38bdc258039af6683a7b880c6fc3855a
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-17 15:24:12 +0200

    task-loop

--- Python-operators.py
+++ Python-operators.py
@@ -6,3 +6,14 @@ if name == 'anas' and age == 21 :
 if name == 'anas' or age == 21 :
     print ('you are not anas')
     
+
+
+x = 500
+print ('x > 400') if x > 400 else print (x)
+
+
+
+age = 20
+username = 'admin'
+ff = True if age >18 and username == 'admin' else False
+print (ff)
---
+++ python-basics-loop.py
@@ -0,0 +1,19 @@
+#while loop
+...
+
+x = 0
+while x < 10 :
+    print(x)
+    x +=1
+    
+...
+
+x = 1
+while x <= 5:
+    y = 1
+    while y <=10 :
+        print(f'{x}X{y} = {x*y}')
+        y +=1
+
+    print('---------------')
+    x += 1
