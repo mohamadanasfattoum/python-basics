b68f6a3eb0e8e918d745561c31d08ae1cd600736
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-22 21:36:40 +0200

    03-python-basics-list-end

--- 03-python-basics.py
+++ 03-python-basics.py
@@ -10,7 +10,7 @@ print('mohamad \nanas')
 '''
 
 text = 'Python Data Types Tasks'
-
+'''
 a=text.upper()
 print(a)
 
@@ -28,3 +28,53 @@ print(dd)
 
 ddd=text.split(' ')
 print(ddd)
+'''
+# list
+'''
+print(text[0])
+print(text[-1])
+print(text[-2])
+print(text[0:5])
+print(text[:5])
+print(text[6:])
+'''
+
+l= [10,11,12,13,14,15,16,17,18,19,20]
+
+print(l[0])
+
+l[0]=100
+
+print(l)
+
+l.append(100)
+print(l)
+
+l.insert(0,200)
+print(l)
+
+l.remove(200)
+print(l)
+
+l.reverse()
+print(l)
+
+
+l.sort()
+print(l)
+
+
+l.sort(reverse=True)
+print(l)
+
+
+l.pop()
+print(l)
+
+
+
+
+
+
+
+
