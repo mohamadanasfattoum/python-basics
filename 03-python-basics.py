1093e5aff466ca6ec8df2ba1088d117e75622b8c
Mohamad anas fattoum <samaanas30@gmail.com>
2023-06-22 19:46:22 +0200

    03-python-basics-text.upper

--- 03-python-basics.py
+++ 03-python-basics.py
@@ -4,5 +4,12 @@ f_name = 'anas'
 l_name = 'fattoum'
 print(f'{f_name} {l_name}')
 '''
+'''
 print('mohamad \t anas')
 print('mohamad \nanas')
+'''
+
+text = 'Python Data Types Tasks'
+
+a=text.upper()
+print(a)
