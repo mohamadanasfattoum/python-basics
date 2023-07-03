ef3691bc0dd739253ed55eb08057b820833d3d35
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-03 16:53:53 +0200

    create-class-student-end

--- 04-python-basics.py
+++ 04-python-basics.py
@@ -63,5 +63,40 @@ s1.create_student('anas')
         '''
     def __init__(self,name):
         print(f'Welcome {name}')
+        self.marks=[]
+
+    def add_mark(self,mark):
+        self.marks.append(mark)
+        print(self.marks)
+
+
+    def get_avg(self):
+        avg = sum(self.marks)/len(self.marks)
+        print(avg)
+
+
 
 s1 = student('anas')
+
+s1.add_mark(20)
+s1.add_mark(50)
+s1.add_mark(40)
+s1.add_mark(90)
+s1.add_mark(30)
+s1.get_avg()
+
+
+
+
+
+
+
+
+s2 = student('ahmad')
+
+s2.add_mark(30)
+s2.add_mark(20)
+s2.add_mark(80)
+s2.add_mark(70)
+s2.add_mark(10)
+s2.get_avg()
