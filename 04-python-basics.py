f9064d158f652e5496a16172de5588e2b637e99e
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-03 12:33:50 +0200

    D.mro

--- 04-python-basics.py
+++ 04-python-basics.py
@@ -1,3 +1,5 @@
+
+'''
 class A:
     def do(self):
         print('in A')
@@ -16,3 +18,28 @@ class D(B,C):
 
 n = D()
 n.do()
+
+
+'''
+
+
+class A:
+    def do(self):
+        print('in A')
+
+class B(A):
+    pass
+class C:
+    def do(self):
+        print('in C')
+
+class D(C,B):
+    pass
+
+
+
+
+n = D()
+n.do()
+
+print(D.mro())
