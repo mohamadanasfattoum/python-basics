700d3b5990ebe7b23469737dc67918390a85cdfd
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-06 23:29:10 +0200

    add-examples-1-for-and-for-with-range

--- Edabit.py
+++ Edabit.py
@@ -8,9 +8,11 @@ def addieren(x,y):
 
 print (addieren(2,6))   
 '''
-
+'''
 def addieren(x,y):
     return x+y
 print(addieren(3,8))
 
+'''
     
+# -2-task
---
+++ examples.py
@@ -0,0 +1,9 @@
+
+
+# -1 example
+
+for x in [1,10]: print(x)
+
+
+for x in range(1,11): print(x)
+
