397632b378c0b44490bada8637308a321398dbc6
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-08 13:28:06 +0200

    add-Functions-examples

--- examples.py
+++ examples.py
@@ -2,8 +2,50 @@
 
 # -1 example
 
-for x in [1,10]: print(x)
+#for x in [1,10]: print(x)
 
 
-for x in range(1,11): print(x)
+#for x in range(0,11,2): print(x)
+##Functions
+
+'''
+def getFirstValue(x,y,z):
+    print(x)
+getFirstValue(1,2,3)
+'''
+'''
+
+def addition(x,y):
+    print(x+y)
+    return
+addition(1,2)
+'''
+
+'''
+def addition(num):
+    print(num+1)
+    return
+addition(0)
+'''
+'''
+
+def addition(num): return print(num+1)
+addition(5)
+'''
+'''
+def addition(num):
+    return print(num+1)
+addition(num=5)
+'''
+''''
+def addition(a,b):
+    return print(a+b)
+addition(a=1,b=2)
+'''
+def sum_user(name,age):
+    print('Name : ' , name)
+    print('Age : ' , age)
+    return
+sum_user(name= 'haneen' , age= 25)
+sum_user(name= 'anas' , age= 25)
 
