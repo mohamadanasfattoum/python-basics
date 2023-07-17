869069e1e8679067c58dc0582ef582faaefd0873
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-17 22:30:08 +0200

    add-try-with-input-and-map-task-hackerrank

--- try.py
+++ try.py
@@ -1,8 +1,17 @@
-a= int(input('enter n: '))
-b= int(input('enter n: '))
+'''
+a= int(input('enter a-n: '))
+b= int(input('enter b-n: '))
 def excep_defi(a,b):
-    return a+b
+    return a*b
 numbers =list(range(a,b))
 result= map(excep_defi,numbers)
 print(list(result))
-    
+'''  
+
+
+for i in range(int(input('enter n: '))):
+    try:
+        a, b = map(int, input('enter n: ').split())
+        print(int(a//b))
+    except Exception as e:
+        print("Error Code:",e)
