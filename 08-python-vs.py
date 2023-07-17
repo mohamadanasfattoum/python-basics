a91f8e84bbd2e5b1285784eaa2fb4d65ea70e170
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-17 22:32:03 +0200

    08-python-vs-stop-

--- 08-python-vs.py
+++ 08-python-vs.py
@@ -29,12 +29,4 @@ print(list(result))
 '''
 
 
-def excep_defi(a,b):
-    a= int(input('enter n: '))
-    b= int(input('enter n: '))    
-    return a//b
-
-numbers =list(range(a,b))
-result= map(excep_defi,numbers)
-print(list(result))
     
